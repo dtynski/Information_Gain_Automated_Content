07220e24186580fa081617ecc277cff18ead6555
Kristin M Tynski <kristin@frac.tl>
2023-12-12 12:11:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ def sanitize_url(url):
     sanitized = re.sub(r'[^\w\-_\. ]', '_', url)  # Replace non-alphanumeric characters with '_'
     return sanitized
 
-def analyze_articles(thread_id, file_ids):
+def analyze_articles(thread_id, file_ids,query):
     notes = []
     individual_file_ids = []  # List to store individual file IDs
     uploaded_notes_ids = []
@@ -371,7 +371,7 @@ def main():
             file_ids = [(str(file_id), link) for file_id, link in file_ids_attempt if file_id is not None and isinstance(file_id, str)]
 
             thread_id = client.beta.threads.create().id
-            back_from_analyze = analyze_articles(thread_id, file_ids)
+            back_from_analyze = analyze_articles(thread_id, file_ids,query)
             aggregated_notes_file_path = back_from_analyze[0]
             uploaded_file_ids = back_from_analyze[1]
 
