70df0553ef09b55669813c1aa9c0c099a5b9f548
Kristin M Tynski <kristin@frac.tl>
2023-12-12 14:06:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -495,8 +495,25 @@ def main():
         df_outline = pd.DataFrame(outline)
         final_outline_file_path = 'final_outline.txt'
 
-        with open(final_outline_file_path, 'w') as file:
-            file.write(outline[-1])
+        if outline:
+            final_outline_file_path = 'final_outline.txt'  # File path for saving
+
+            # Writing the content to the text file
+            with open(final_outline_file_path, 'w') as file:
+                for item in outline:
+                    file.write(item + '\n')  # Write each item in outline to the file
+        
+            # Confirming file creation
+            if os.path.exists(final_outline_file_path):
+                st.success(f"Outline saved to {final_outline_file_path}")
+                # File download option
+                with open(final_outline_file_path, "rb") as file:
+                    st.download_button("Download Final Outline", file, file_name="final_outline.txt")
+                    else:
+                        st.error("Failed to save the outline file.")
+                else:
+                    st.error("No outline data to save.")
+            
         outline_file_path = "all_outlines.csv"
         df_outline.to_csv(outline_file_path, sep='\t', index=False)
 
